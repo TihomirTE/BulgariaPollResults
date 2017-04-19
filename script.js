83c8e83c230ff5d479fb7964a74b70ffd63cbcd4
Martin Kanovski <mkanovsk@progress.com>
2017-04-19 17:13:23 +0300

    enrichment of the map data

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var colors = ["#E96D63", "#7FCA9F", "#F4BA70", "#85C1F5", "#4A789C", "#13A1CB", "#728CB0", "#C296B6"];
+var colors = ["#E96D63", "#7FCA9F", "#F4BA70", "#85C1F5", "#4A789C", "#13A1CB", "#728CB0", "#C296B6", "#66ff99", "#ff99ff"];
 
 $("#map").kendoMap({
     center: [42.578129, 25.091893],
@@ -13,8 +13,8 @@ $("#map").kendoMap({
         }
     }],
     shapeCreated: function(e) {
-        console.log(e.shape.dataItem.provnum_ne);
-        var provNum = e.shape.dataItem.provnum_ne;
+        console.log(e.shape.dataItem.id);
+        var provNum = e.shape.dataItem.id;
         if (provNum) {
             e.shape.fill(colors[provNum % colors.length]);
         }
