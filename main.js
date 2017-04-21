ed83ee4eddfcba19c479993265fb2bca7f2217ac
Martin Kanovski <mkanovsk@progress.com>
2017-04-21 12:55:46 +0300

    initial files setup

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log('it works');
+
+import { createMap } from './tab_karta/script_tab_karta.js';
+
+createMap();
--- tab_karta/script_tab_karta.js
+++ tab_karta/script_tab_karta.js
@@ -1,39 +1,49 @@
-var colors = ["#E96D63", "#7FCA9F", "#F4BA70", "#85C1F5", "#4A789C", "#13A1CB", "#728CB0", "#C296B6", "#66ff99", "#ff99ff", "#ff6699", "#53e9d0"];
+function createMap() {
+    
+    let colors = ["#E96D63", "#7FCA9F", "#F4BA70", "#85C1F5", "#4A789C", "#13A1CB", "#728CB0", "#C296B6", "#66ff99", "#ff99ff", "#ff6699", "#53e9d0"];
 
-$("#map").kendoMap({
-    center: [42.578129, 25.091893],
-    zoom: 8,
-    layerDefaults: {
-            shape: {
-                style: {
-                    stroke: {
-                        color: "white",
-                        width: 1,
-                        dashType: "solid",
-                        opacity: 1
+    let $divContainer = $("#tab-content");
+
+    $divContainer.width(650);
+    $divContainer.height(500);
+
+    $divContainer.kendoMap({
+        center: [42.578129, 25.091893],
+        zoom: 7,
+        layerDefaults: {
+                shape: {
+                    style: {
+                        stroke: {
+                            color: "white",
+                            width: 1,
+                            dashType: "solid",
+                            opacity: 1
+                        }
                     }
                 }
+            },
+        layers: [{
+            type: "shape",
+            dataSource: {
+                type: "geojson",
+                transport: {
+                    read: "./dependencies/bulgaria_map.json"
+                }
+            }
+        }],
+        shapeCreated: function(e) {
+            var provNum = e.shape.dataItem.id;
+            if (provNum) {
+                e.shape.fill(colors[provNum % colors.length]);
             }
         },
-    layers: [{
-        type: "shape",
-        dataSource: {
-            type: "geojson",
-            transport: {
-                read: "./dependencies/bulgaria_map.json"
+        shapeMouseEnter: function(e) {
+                
+                e.shape.stroke("red");
+                
             }
-        }
-    }],
-    shapeCreated: function(e) {
-        var provNum = e.shape.dataItem.id;
-        if (provNum) {
-            e.shape.fill(colors[provNum % colors.length]);
-        }
-    },
-    shapeMouseEnter: function(e) {
-            
-            e.shape.stroke("red");
-            
-        }
-    
-});
+        
+    });
+}
+
+export { createMap };
