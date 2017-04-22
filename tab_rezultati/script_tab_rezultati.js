9e37a142ab24abae6d78f8b9d8c43e63734c648d
TihomirTE <tihomirteofilov@gmail.com>
2017-04-22 20:36:10 +0300

    Separate variable data

--- tab_rezultati/script_tab_rezultati.js
+++ tab_rezultati/script_tab_rezultati.js
@@ -1,3 +1,47 @@
+  let results = [{
+      category: "ПП Герб",
+      value: 32.65,
+      color: "#0000FF"
+  }, {
+      category: "Не подкрепям никого",
+      value: 2.50,
+      color: "#FFFFFF"
+  }, {
+      category: "БСП за България",
+      value: 27.20,
+      color: "#FF0000"
+  }, {
+      category: "Да България",
+      value: 2.88,
+      color: "#4C9F00"
+  }, {
+      category: "Нова Република",
+      value: 2.48,
+      color: "#808080"
+  }, {
+      category: "Обединени Патриоти",
+      value: 9.07,
+      color: "#ffd600"
+  }, {
+      category: "ДПС",
+      value: 8.99,
+      color: "#B200FF"
+  }, {
+      category: "Воля",
+      value: 4.15,
+      color: "#00FFFF"
+  }, {
+      category: "Реформаторски блок",
+      value: 3.06,
+      color: "#4800FF"
+  }, {
+      category: "Дост",
+      value: 2.86,
+      color: "#FF00DC"
+  }];
+
+
+
   function pollResults() {
       $("#chart").kendoChart({
           title: {
@@ -20,47 +64,7 @@
           series: [{
               type: "pie",
               startAngle: 150,
-              data: [{
-                  category: "ПП Герб",
-                  value: 32.65,
-                  color: "#0000FF"
-              }, {
-                  category: "Не подкрепям никого",
-                  value: 2.50,
-                  color: "#FFFFFF"
-              }, {
-                  category: "БСП за България",
-                  value: 27.20,
-                  color: "#FF0000"
-              }, {
-                  category: "Да България",
-                  value: 2.88,
-                  color: "#4C9F00"
-              }, {
-                  category: "Нова Република",
-                  value: 2.48,
-                  color: "#808080"
-              }, {
-                  category: "Обединени Патриоти",
-                  value: 9.07,
-                  color: "#ffd600"
-              }, {
-                  category: "ДПС",
-                  value: 8.99,
-                  color: "#B200FF"
-              }, {
-                  category: "Воля",
-                  value: 4.15,
-                  color: "#00FFFF"
-              }, {
-                  category: "Реформаторски блок",
-                  value: 3.06,
-                  color: "#4800FF"
-              }, {
-                  category: "Дост",
-                  value: 2.86,
-                  color: "#FF00DC"
-              }]
+              data: results
           }],
           tooltip: {
               visible: true,
